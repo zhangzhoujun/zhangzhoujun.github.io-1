7a9473dcdc48954b63893c7f258fe63a2713e7e6
张舟俊 <271756926@qq.com>
2016-11-22 19:57:25 +0800

    修改http请求支持delete请求

--- MyVolley/app/src/main/java/com/android/myvolley/volley/toolbox/HurlStack.java
+++ MyVolley/app/src/main/java/com/android/myvolley/volley/toolbox/HurlStack.java
@@ -61,7 +61,7 @@ public class HurlStack implements HttpStack {
         public String rewriteUrl(String originalUrl);
     }
 
-    private final UrlRewriter mUrlRewriter;
+    private final UrlRewriter      mUrlRewriter;
     private final SSLSocketFactory mSslSocketFactory;
 
     public HurlStack() {
@@ -69,15 +69,18 @@ public class HurlStack implements HttpStack {
     }
 
     /**
-     * @param urlRewriter Rewriter to use for request URLs
+     * @param urlRewriter
+     *            Rewriter to use for request URLs
      */
     public HurlStack(UrlRewriter urlRewriter) {
         this(urlRewriter, null);
     }
 
     /**
-     * @param urlRewriter Rewriter to use for request URLs
-     * @param sslSocketFactory SSL factory to use for HTTPS connections
+     * @param urlRewriter
+     *            Rewriter to use for request URLs
+     * @param sslSocketFactory
+     *            SSL factory to use for HTTPS connections
      */
     public HurlStack(UrlRewriter urlRewriter, SSLSocketFactory sslSocketFactory) {
         mUrlRewriter = urlRewriter;
@@ -85,8 +88,7 @@ public class HurlStack implements HttpStack {
     }
 
     @Override
-    public HttpResponse performRequest(Request<?> request, Map<String, String> additionalHeaders)
-            throws IOException, AuthFailureError {
+    public HttpResponse performRequest(Request<?> request, Map<String, String> additionalHeaders) throws IOException, AuthFailureError {
         String url = request.getUrl();
         HashMap<String, String> map = new HashMap<String, String>();
         map.putAll(request.getHeaders(request.getHeadType()));
@@ -108,12 +110,13 @@ public class HurlStack implements HttpStack {
         ProtocolVersion protocolVersion = new ProtocolVersion("HTTP", 1, 1);
         int responseCode = connection.getResponseCode();
         if (responseCode == -1) {
-            // -1 is returned by getResponseCode() if the response code could not be retrieved.
-            // Signal to the caller that something was wrong with the connection.
+            // -1 is returned by getResponseCode() if the response code could
+            // not be retrieved.
+            // Signal to the caller that something was wrong with the
+            // connection.
             throw new IOException("Could not retrieve response code from HttpUrlConnection.");
         }
-        StatusLine responseStatus = new BasicStatusLine(protocolVersion,
-                connection.getResponseCode(), connection.getResponseMessage());
+        StatusLine responseStatus = new BasicStatusLine(protocolVersion, connection.getResponseCode(), connection.getResponseMessage());
         BasicHttpResponse response = new BasicHttpResponse(responseStatus);
         response.setEntity(entityFromConnection(connection));
         for (Entry<String, List<String>> header : connection.getHeaderFields().entrySet()) {
@@ -126,7 +129,9 @@ public class HurlStack implements HttpStack {
     }
 
     /**
-     * Initializes an {@link HttpEntity} from the given {@link HttpURLConnection}.
+     * Initializes an {@link HttpEntity} from the given
+     * {@link HttpURLConnection}.
+     * 
      * @param connection
      * @return an HttpEntity populated with data from <code>connection</code>.
      */
@@ -154,6 +159,7 @@ public class HurlStack implements HttpStack {
 
     /**
      * Opens an {@link HttpURLConnection} with parameters.
+     * 
      * @param url
      * @return an open connection
      * @throws IOException
@@ -169,62 +175,64 @@ public class HurlStack implements HttpStack {
 
         // use caller-provided custom SslSocketFactory, if any, for HTTPS
         if ("https".equals(url.getProtocol()) && mSslSocketFactory != null) {
-            ((HttpsURLConnection)connection).setSSLSocketFactory(mSslSocketFactory);
+            ((HttpsURLConnection) connection).setSSLSocketFactory(mSslSocketFactory);
         }
 
         return connection;
     }
 
     @SuppressWarnings("deprecation")
-    /* package */ static void setConnectionParametersForRequest(HttpURLConnection connection,
-            Request<?> request) throws IOException, AuthFailureError {
+    /* package */ static void setConnectionParametersForRequest(HttpURLConnection connection, Request<?> request) throws IOException, AuthFailureError {
         switch (request.getMethod()) {
-            case Method.DEPRECATED_GET_OR_POST:
-                // This is the deprecated way that needs to be handled for backwards compatibility.
-                // If the request's post body is null, then the assumption is that the request is
-                // GET.  Otherwise, it is assumed that the request is a POST.
-                byte[] postBody = request.getPostBody();
-                if (postBody != null) {
-                    // Prepare output. There is no need to set Content-Length explicitly,
-                    // since this is handled by HttpURLConnection using the size of the prepared
-                    // output stream.
-                    connection.setDoOutput(true);
-                    connection.setRequestMethod("POST");
-                    connection.addRequestProperty(HEADER_CONTENT_TYPE,
-                            request.getPostBodyContentType());
-                    DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-                    out.write(postBody);
-                    out.close();
-                }
-                break;
-            case Method.GET:
-                // Not necessary to set the request method because connection defaults to GET but
-                // being explicit here.
-                connection.setRequestMethod("GET");
-                break;
-            case Method.DELETE:
-                connection.setRequestMethod("DELETE");
-                addBodyIfExists(connection, request);
-                break;
-            case Method.POST:
+        case Method.DEPRECATED_GET_OR_POST:
+            // This is the deprecated way that needs to be handled for backwards
+            // compatibility.
+            // If the request's post body is null, then the assumption is that
+            // the request is
+            // GET. Otherwise, it is assumed that the request is a POST.
+            byte[] postBody = request.getPostBody();
+            if (postBody != null) {
+                // Prepare output. There is no need to set Content-Length
+                // explicitly,
+                // since this is handled by HttpURLConnection using the size of
+                // the prepared
+                // output stream.
+                connection.setDoOutput(true);
                 connection.setRequestMethod("POST");
-                addBodyIfExists(connection, request);
-                break;
-            case Method.PUT:
-                connection.setRequestMethod("PUT");
-                addBodyIfExists(connection, request);
-                break;
-            case Method.PATCH:
-                connection.setRequestMethod("PATCH");
-                addBodyIfExists(connection, request);
-                break;
-            default:
-                throw new IllegalStateException("Unknown method type.");
+                connection.addRequestProperty(HEADER_CONTENT_TYPE, request.getPostBodyContentType());
+                DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                out.write(postBody);
+                out.close();
+            }
+            break;
+        case Method.GET:
+            // Not necessary to set the request method because connection
+            // defaults to GET but
+            // being explicit here.
+            connection.setRequestMethod("GET");
+            break;
+        case Method.DELETE:
+            connection.setRequestMethod("DELETE");
+            addBodyIfExists(connection, request);
+            break;
+        case Method.POST:
+            connection.setRequestMethod("POST");
+            addBodyIfExists(connection, request);
+            break;
+        case Method.PUT:
+            connection.setRequestMethod("PUT");
+            addBodyIfExists(connection, request);
+            break;
+        case Method.PATCH:
+            connection.setRequestMethod("PATCH");
+            addBodyIfExists(connection, request);
+            break;
+        default:
+            throw new IllegalStateException("Unknown method type.");
         }
     }
 
-    private static void addBodyIfExists(HttpURLConnection connection, Request<?> request)
-            throws IOException, AuthFailureError {
+    private static void addBodyIfExists(HttpURLConnection connection, Request<?> request) throws IOException, AuthFailureError {
         byte[] body = request.getBody();
         if (body != null) {
             connection.setDoOutput(true);
